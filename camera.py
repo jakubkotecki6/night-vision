6ecadabc8841efc3d9994caa2d1df1e2faa4d3c4
jakub <jakubkotecki6@gmail.com>
2024-07-18 20:22:37 +0200

    camera adjusted window to screen

--- camera.py
+++ camera.py
@@ -1,9 +1,13 @@
 import cv2
 
+cameraWidth, cameraHeight, cameraFramerate = 256, 192, 50
+screenWidth, screenHeight = 320, 192
+moveRight, moveDown = int((720-screenWidth)/2), int((480-screenHeight)/2)
+
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 192)
-cap.set(cv2.CAP_PROP_FPS, 50)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cameraWidth)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cameraHeight)
+cap.set(cv2.CAP_PROP_FPS, cameraFramerate)
 
 while True:
     ret, frame = cap.read()
@@ -11,11 +15,17 @@ while True:
         print("Unable to access camera")
         break
 
+#   Full screen
+#    cv2.namedWindow("camera", cv2.WND_PROP_FULLSCREEN)
+#    cv2.setWindowProperty("camera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+#   Cropped to screen size and centered window
     cv2.namedWindow("camera", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("camera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow('camera', frame)
-    if cv2.waitKey(30) == 27:
-        break
+    cv2.resizeWindow("camera", screenWidth, screenHeight)
+    cv2.moveWindow("camera", moveRight, moveDown)
+
+    key = cv2.waitKey(1)
+    cv2.imshow("camera", frame)
 
 cap.release()
 cv2.destroyAllWindows()
