ca44a211978bfb066c223f7bc5251d7e1f3924f0
jakub <jakubkotecki6@gmail.com>
2024-07-22 20:12:21 +0200

    parameters

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@ import cv2
 
 cameraWidth, cameraHeight, cameraFramerate = 256, 192, 50
 screenWidth, screenHeight = 320, 192
-moveRight, moveDown = int((720-screenWidth)/2), int((480-screenHeight)/2)
+moveRight, moveDown = int((720 - screenWidth) / 2), int((480 - screenHeight) / 2)
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, cameraWidth)
@@ -15,17 +15,18 @@ while True:
         print("Unable to access camera")
         break
 
-#   Full screen
-#    cv2.namedWindow("camera", cv2.WND_PROP_FULLSCREEN)
-#    cv2.setWindowProperty("camera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    #   Full screen
+    #    cv2.namedWindow("camera", cv2.WND_PROP_FULLSCREEN)
+    #    cv2.setWindowProperty("camera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-#   Cropped to screen size and centered window
+    #   Cropped to screen size and centered window
     cv2.namedWindow("camera", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("camera", screenWidth, screenHeight)
     cv2.moveWindow("camera", moveRight, moveDown)
-
-    key = cv2.waitKey(1)
     cv2.imshow("camera", frame)
+
+    if cv2.waitKey(1) != -1:
+        break
 
 cap.release()
 cv2.destroyAllWindows()
--- yolo-object-detection.py
+++ yolo-object-detection.py
@@ -3,10 +3,14 @@ from ultralytics import YOLO
 
 model = YOLO("best.pt")
 
+cameraWidth, cameraHeight, cameraFramerate = 256, 192, 50
+screenWidth, screenHeight = 320, 192
+moveRight, moveDown = int((720 - screenWidth) / 2), int((480 - screenHeight) / 2)
+
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 192)
-cap.set(cv2.CAP_PROP_FPS, 50)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cameraWidth)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cameraHeight)
+cap.set(cv2.CAP_PROP_FPS, cameraFramerate)
 
 while True:
     ret, frame = cap.read()
@@ -17,11 +21,17 @@ while True:
 
     result = model.predict(frame, conf=0.15)
 
+    #   Full screen
+    #    cv2.namedWindow("camera", cv2.WND_PROP_FULLSCREEN)
+    #    cv2.setWindowProperty("camera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    #   Cropped to screen size and centered window
     cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.resizeWindow("frame", screenWidth, screenHeight)
+    cv2.moveWindow("frame", moveRight, moveDown)
     cv2.imshow("frame", result[0].plot())
-    cv2.waitKey(50)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    if cv2.waitKey(1) != -1:
         break
 
 cap.release()
