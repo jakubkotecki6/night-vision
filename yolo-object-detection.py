201a9510d4e91f9c2ba994d816a19564cc7c3f5c
jakubkotecki6 <jakubkotecki6@gmail.com>
2024-10-29 14:47:15 +0100

    Working

--- yolo-object-detection.py
+++ yolo-object-detection.py
@@ -4,8 +4,7 @@ from ultralytics import YOLO
 model = YOLO("best.pt")
 
 cameraWidth, cameraHeight, cameraFramerate = 256, 192, 50
-screenWidth, screenHeight = 320, 192
-moveRight, moveDown = int((720 - screenWidth) / 2), int((480 - screenHeight) / 2)
+confidenceThreshold = 0.35
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, cameraWidth)
@@ -19,17 +18,13 @@ while True:
         print("Failed to capture frame")
         continue
 
-    result = model.predict(frame, conf=0.15)
+    result = model.predict(frame, conf=confidenceThreshold)
 
     #   Full screen
-    cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.namedWindow("detection", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("detection", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-    #   Cropped to screen size and centered window
-    #cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-    #cv2.resizeWindow("frame", screenWidth, screenHeight)
-    #cv2.moveWindow("frame", moveRight, moveDown)
-    cv2.imshow("frame", result[0].plot())
+    cv2.imshow("detection", result[0].plot())
 
     if cv2.waitKey(1) != -1:
         break
